f5ce854c06e8295d6d104f1ee88460d097d79460
Joe Baldwin <joe@humboldttechgroup.com>
2012-12-27 01:40:10 -0600

    Chapter 2 Project Creation

---
+++ Chapter2/RandomPossessions/RandomPossessions/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  RandomPossessions
+//
+//  Created by Joseph Baldwin on 12/27/12.
+//  Copyright (c) 2012 Joe Baldwin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
