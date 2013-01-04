effc97d3149005b5f9e0a8e41b2b9bcfae8d079b
Joe Baldwin <joe@humboldttechgrou.com>
2013-01-04 11:09:29 -0600

    Chapter 8 Start

---
+++ Chapter8/HeavyRotation/HeavyRotation/RotationAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RotationAppDelegate.h
+//  HeavyRotation
+//
+//  Created by Joseph on 1/4/13.
+//  Copyright (c) 2013 Joe Baldwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotationAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
