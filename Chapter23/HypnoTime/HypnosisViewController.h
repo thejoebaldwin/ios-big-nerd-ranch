0859a40ec1dc449b673e3cb3941158dd648685ee
Joe Baldwin <joe@humboldttechgroup.com>
2013-01-16 01:42:25 -0600

    Chapter 23 Start

---
+++ Chapter23/HypnoTime/HypnosisViewController.h
@@ -0,0 +1,13 @@
+//
+//  HypnosisViewController.h
+//  HypnoTime
+//
+//  Created by Joseph Baldwin on 12/29/12.
+//  Copyright (c) 2012 Joe Baldwin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HypnosisViewController : UIViewController
+
+@end
